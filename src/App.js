951ef97a721ae12259f060669ea2c59d89460115
Himanshu Singh <61236944+himanshusingh335@users.noreply.github.com>
2022-05-11 09:03:37 +0530

    🎉 initialize project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import PrimarySearchAppBar from "./appBar";
 
 function App() {
+  const [flightStatus, setFlightStatus] = useState([])
+  const [flightNumber, setFlightNumber] = useState("6E7932")
+  const [date, setDate] = useState("2022-05-11")
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Host': 'aerodatabox.p.rapidapi.com',
+        'X-RapidAPI-Key': '091d3ca15fmshf687b8d71d5b41ep15a8d9jsn26fd0bb2a445'
+      }
+    };
+
+    fetch('https://aerodatabox.p.rapidapi.com/flights/number/' + flightNumber + '/' + date + '', options)
+      .then(response => response.json())
+      .then(response => setFlightStatus(response))
+      .catch(err => console.error(err));
+  }, [])
+  console.log(flightStatus);
+  //console.log(flightStatus[0].greatCircleDistance);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PrimarySearchAppBar />
   );
 }
 
